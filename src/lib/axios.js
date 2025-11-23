d14304f70e5165b567e22910d229dd4c19f23905
Deep Bhandalkar <157112839+devp0294@users.noreply.github.com>
2025-11-23 14:47:45 +0530

    Update axios baseURL for development environment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://e-commerce-backend-3wmn.onrender.com/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://e-commerce-backend-bipl.onrender.com/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
