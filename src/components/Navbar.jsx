7e43bf00a8aebf8f22863014b3b60242282bba10
devp0294 <devpbhandalkar@gmail.com>
2025-11-23 14:01:05 +0530

    Updated project with latest changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
+					<Link to='https://github.com/devp0294/E-Commerce-Frontend.git' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
 						E-Commerce
 					</Link>
 
