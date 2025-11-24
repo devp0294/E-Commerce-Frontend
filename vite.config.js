84cf60088640fb87f443e55d4809cbba9c9edf9a
Deep Bhandalkar <157112839+devp0294@users.noreply.github.com>
2025-11-24 11:48:17 +0530

    Add build configuration to vite.config.js
    Added build configuration with chunk size warning limit.

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // in KB (e.g., 2000 KB = 2 MB)
+  },
 });
