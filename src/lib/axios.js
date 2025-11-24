acce348cdf7c2fe89e7a596589c5bdd18297ac78
Deep Bhandalkar <157112839+devp0294@users.noreply.github.com>
2025-11-24 11:38:10 +0530

    Update axios baseURL for production environment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api" // your local backend during dev
-    : "https://e-commerce-backend-bipl.onrender.com/api", // production backend
+  baseURL: "https://e-commerce-backend-f0np.onrender.com", // production backend
   withCredentials: true, // send cookies for auth
 });
 
